d03a5a5d107d6fc2c4cd8484514e2857ab075036
Alfred Melson <avm@hmtgp.com>
2022-02-08 10:16:50 +0200

    removed prettier conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,9 +20,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    // 'plugin:prettier/recommended',
     'prettier'
   ],
 
