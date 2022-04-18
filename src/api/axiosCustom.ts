0242f6590df441fdd6c742423a93b5e685b54338
Alfred Melson <avm@hmtgp.com>
2022-04-18 05:26:45 +0300

    updated endpoint

--- src/api/axiosCustom.ts
+++ src/api/axiosCustom.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:9003'
+const BASE_URL = 'https://jngg1sk37g.execute-api.eu-central-1.amazonaws.com/'
+// const DEV_URL = 'http://localhost:9003'
 
 export default axios.create({
   baseURL: BASE_URL
