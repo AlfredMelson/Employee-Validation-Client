882402e6bd3a13002c32f2e3a2440b19f8d92d38
Alfred Melson <avm@hmtgp.com>
2022-04-24 08:56:34 +0300

    updated new EBS url

--- src/api/axiosCustom.ts
+++ src/api/axiosCustom.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'https://emplverify.eu-central-1.elasticbeanstalk.com/'
+const BASE_URL = 'https://emplVerify.eu-central-1.elasticbeanstalk.com'
 // const BASE_URL = 'http://localhost:9003'
 
 export default axios.create({
